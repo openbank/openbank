bc55080dab4ca5ff6ac1ebb6404064dd6e9ca9d7
Daniel Martí <mvdan@mvdan.cc>
2019-07-16 13:16:58 +0900

    more

--- deps.sh
+++ deps.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-go get "$@" \
+#!/bin/sh
+
+go get \
 	github.com/golang/protobuf/protoc-gen-go \
 	github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway \
 	github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger \
-	github.com/gunk/gunk@master \
+	github.com/gunk/gunk \
 	|| exit 1
