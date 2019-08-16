b58bd92b15ebc458ad364dfb957a72e4cf7157da
Novian Kristianto <novian.kristianto@brank.as>
2019-08-16 14:54:40 +0700

    auth : Add auth scope list
    Add Scope for profile and branch. Update scope URL from bank.to to
    auth.bnk.to

--- v1/auth/scope.go
+++ v1/auth/scope.go
@@ -4,38 +4,62 @@ package auth
 
 const (
 	// TranscationCreate scope for the transaction read operation.
-	Scope_TransactionCreate = "https://bank.to/transaction.create"
+	Scope_TransactionCreate = "https://auth.bnk.to/transaction.create"
 
 	// TransactionRead scope for the transaction read operation.
-	Scope_TransactionRead = "https://bank.to/transaction.read"
+	Scope_TransactionRead = "https://auth.bnk.to/transaction.read"
 
 	// TransactionUpdate scope for the transaction update operation.
-	Scope_TransactionUpdate = "https://bank.to/transaction.update"
+	Scope_TransactionUpdate = "https://auth.bnk.to/transaction.update"
 
 	// TransactionDelete scope for the transaction delete operation.
-	Scope_TransactionDelete = "https://bank.to/transaction.delete"
+	Scope_TransactionDelete = "https://auth.bnk.to/transaction.delete"
 
 	// AccountsCreate scope fot the account crate operation.
-	Scope_AccountsCreate = "https://bank.to/account.create"
+	Scope_AccountsCreate = "https://auth.bnk.to/account.create"
 
 	// AccountsRead scope for the account read account operation.
-	Scope_AccountsRead = "https://bank.to/account.read"
+	Scope_AccountsRead = "https://auth.bnk.to/account.read"
 
 	// AccountsUpdate scope for the account update operation.
-	Scope_AccountsUpdate = "https://bank.to/account.update"
+	Scope_AccountsUpdate = "https://auth.bnk.to/account.update"
 
 	// AccountsDelete scope for the account delete operation.
-	Scope_AccountsDelete = "https://bank.to/account.delete"
+	Scope_AccountsDelete = "https://auth.bnk.to/account.delete"
 
 	// CardsCreate scope for the card create operation.
-	Scope_CardsCreate = "https://bank.to/card.create"
+	Scope_CardsCreate = "https://auth.bnk.to/card.create"
 
 	// CardsRead scope for the card read operation.
-	Scope_CardsRead = "https://bank.to/card.read"
+	Scope_CardsRead = "https://auth.bnk.to/card.read"
 
 	// CardsUpdate scope for the card update operation.
-	Scope_CardsUpdate = "https://bank.to/card.update"
+	Scope_CardsUpdate = "https://auth.bnk.to/card.update"
 
 	// CardsDelete scope for the card delete operation.
-	Scope_CardsDelete = "https://bank.to/card.delete"
+	Scope_CardsDelete = "https://auth.bnk.to/card.delete"
+
+	// ProfileCreate scope for the profile create operation.
+	Scope_ProfileCreate = "https://auth.bnk.to/profile.create"
+
+	// ProfileRead scope for the profile read operation.
+	Scope_ProfileRead = "https://auth.bnk.to/profile.read"
+
+	// ProfileUpdate scope for the profile update operation.
+	Scope_ProfileUpdate = "https://auth.bnk.to/profile.update"
+
+	// ProfileDelete scope for the profile delete operation.
+	Scope_ProfileDelete = "https://auth.bnk.to/profile.delete"
+
+	// BranchCreate scope for the branch create operation.
+	Scoce_BranchCreate = "https://auth.bnk.to/branch.create"
+
+	// BranchRead scope for the branch read operation.
+	Scoce_BranchRead = "https://auth.bnk.to/branch.read"
+
+	// BranchUpdate scope for the branch update operation.
+	Scoce_BranchUpdate = "https://auth.bnk.to/branch.update"
+
+	// BranchDelete scope fot the branch delete operation.
+	Scoce_BranchDelete = "https://auth.bnk.to/branch.delete"
 )
