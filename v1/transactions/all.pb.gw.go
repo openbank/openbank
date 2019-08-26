a9d6a12d73cbe081160e1bafbfe4c2ec2f2c249b
Ghazni Nattarshah <ghazni.nattarshah@brank.as>
2019-08-26 18:33:03 +0530

    Updated the REST resource path for fetching transactions by account endpoint to /v1/accounts/<account-id>/transactions
    OB-985

--- v1/transactions/all.pb.gw.go
+++ v1/transactions/all.pb.gw.go
@@ -76,12 +76,30 @@ func request_TransactionService_GetTransactions_0(ctx context.Context, marshaler
 }
 
 var (
-	filter_TransactionService_GetTransactionsByAccount_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+	filter_TransactionService_GetTransactionsByAccount_0 = &utilities.DoubleArray{Encoding: map[string]int{"AccountID": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
 
 func request_TransactionService_GetTransactionsByAccount_0(ctx context.Context, marshaler runtime.Marshaler, client TransactionServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetTransactionsByAccountRequest
 	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["AccountID"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "AccountID")
+	}
+
+	protoReq.AccountID, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "AccountID", err)
+	}
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
@@ -312,7 +330,7 @@ var (
 
 	pattern_TransactionService_GetTransactions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "transactions"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_TransactionService_GetTransactionsByAccount_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "transactions", "account"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_TransactionService_GetTransactionsByAccount_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"v1", "accounts", "AccountID", "transactions"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_TransactionService_CreateTransaction_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "transactions"}, "", runtime.AssumeColonVerbOpt(true)))
 
