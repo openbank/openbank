1eb5eec31b9c02b5160484be9e948d0198058db6
Novian Kristianto <novian.kristianto@brank.as>
2019-10-25 17:25:19 +0700

    v1/customer: Add Address to customer and social media handle API
    Extend the customer API with add AddAddressToCustomer and
    AddSocialMediaHandle api definition.

--- v1/customer/all.md
+++ v1/customer/all.md
@@ -6,6 +6,199 @@ Provides create and read operations on the customer resource.
 
 * Base Path ``
 
+## Add address to the customer.
+
+Add address to the customer.
+
+```sh
+curl -X POST \
+	/v1/customers/{CustomerID}/address \
+	-H 'Authorization: Bearer USE_YOUR_TOKEN' \
+	-d '{
+		"customer_id": "string",
+		"line_1": "string",
+		"line_2": "string",
+		"line_3": "string",
+		"city": "string",
+		"county": "string",
+		"state": "string",
+		"postcode": "string",
+		"country_code": "string",
+		"tags": "[]string",
+		"status": "string"
+	}'
+```
+{{snippet addaddressto_customer []}}
+
+### HTTP Request
+
+`POST /v1/customers/{CustomerID}/address`
+
+### Query Parameters
+
+| Name       | Type   | Description |
+|------------|--------|-------------|
+| CustomerID | string |             |
+
+### Body Parameters
+
+| Name        | Type     | Description |
+|-------------|----------|-------------|
+| CustomerID  | string   |             |
+| Line1       | string   |             |
+| Line2       | string   |             |
+| Line3       | string   |             |
+| City        | string   |             |
+| County      | string   |             |
+| State       | string   |             |
+| Postcode    | string   |             |
+| CountryCode | string   |             |
+| Tags        | []string |             |
+| Status      | string   |             |
+
+### Responses
+
+#### Response body
+
+| Name              | Type      | Description |
+|-------------------|-----------|-------------|
+| CustomerAddressID | string    |             |
+| CustomerID        | string    |             |
+| Line1             | string    |             |
+| Line2             | string    |             |
+| Line3             | string    |             |
+| City              | string    |             |
+| County            | string    |             |
+| State             | string    |             |
+| Postcode          | string    |             |
+| CountryCode       | string    |             |
+| Tags              | []string  |             |
+| Status            | string    |             |
+| InsertDate        | Timestamp |             |
+
+##### Objects
+
+###### Timestamp
+
+| Name    | Type  | Description |
+|---------|-------|-------------|
+| seconds | int64 |             |
+| nanos   | int32 |             |
+
+Example:
+
+```json
+{
+  "customer_address_id": "string",
+  "customer_id": "string",
+  "line_1": "string",
+  "line_2": "string",
+  "line_3": "string",
+  "city": "string",
+  "county": "string",
+  "state": "string",
+  "postcode": "string",
+  "country_code": "string",
+  "tags": "[]string",
+  "status": "string",
+  "insert_date": {
+    "seconds": "int64",
+    "nanos": "int32"
+  }
+}
+```
+#### Response codes
+
+| Status | Description                                                                            |
+|--------|----------------------------------------------------------------------------------------|
+| 201    | Account application created successfully.                                              |
+| 400    | Returned when the request body is malformatted or does not match the expected request. |
+| 401    | Returned when the request does not contains the user's credentials.                    |
+| 403    | Returned when the user does not have permission to access the resource.                |
+| 404    | Returned when the resource is not found.                                               |
+| 500    | Returned when an unexpected error occured on the server side.                          |
+
+## Add social media handle
+
+Add social media handle for the customer
+
+```sh
+curl -X POST \
+	/v1/customers/{CustomerID}/social_media_handles \
+	-H 'Authorization: Bearer USE_YOUR_TOKEN' \
+	-d '{
+		"customer_id": "string",
+		"customer_number": "string",
+		"type": "string",
+		"handle": "string",
+		"date_added": {
+			"seconds": "int64",
+			"nanos": "int32"
+		},
+		"date_activated": {
+			"seconds": "int64",
+			"nanos": "int32"
+		}
+	}'
+```
+{{snippet addsocialmedia_handle []}}
+
+### HTTP Request
+
+`POST /v1/customers/{CustomerID}/social_media_handles`
+
+### Query Parameters
+
+| Name       | Type   | Description |
+|------------|--------|-------------|
+| CustomerID | string |             |
+
+### Body Parameters
+
+| Name           | Type      | Description |
+|----------------|-----------|-------------|
+| CustomerID     | string    |             |
+| CustomerNumber | string    |             |
+| Type           | string    |             |
+| Handle         | string    |             |
+| DateAdded      | Timestamp |             |
+| DateActivated  | Timestamp |             |
+
+##### Objects
+
+###### Timestamp
+
+| Name    | Type  | Description |
+|---------|-------|-------------|
+| seconds | int64 |             |
+| nanos   | int32 |             |
+
+### Responses
+
+#### Response body
+
+| Name    | Type   | Description |
+|---------|--------|-------------|
+| Success | string |             |
+
+Example:
+
+```json
+{
+  "success": "string"
+}
+```
+#### Response codes
+
+| Status | Description                                                                            |
+|--------|----------------------------------------------------------------------------------------|
+| 201    | Social media added successfully.                                                       |
+| 400    | Returned when the request body is malformatted or does not match the expected request. |
+| 401    | Returned when the request does not contains the user's credentials.                    |
+| 403    | Returned when the user does not have permission to access the resource.                |
+| 404    | Returned when the resource is not found.                                               |
+| 500    | Returned when an unexpected error occured on the server side.                          |
+
 ## Create a customer
 
 Creates a new customer
--- v1/customer/all.pb.gw.go
+++ v1/customer/all.pb.gw.go
@@ -28,6 +28,76 @@ var _ status.Status
 var _ = runtime.String
 var _ = utilities.NewDoubleArray
 
+func request_CustomerService_AddAddressToCustomer_0(ctx context.Context, marshaler runtime.Marshaler, client CustomerServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq AddAddressToCustomerRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["CustomerID"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "CustomerID")
+	}
+
+	protoReq.CustomerID, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "CustomerID", err)
+	}
+
+	msg, err := client.AddAddressToCustomer(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func request_CustomerService_AddSocialMediaHandle_0(ctx context.Context, marshaler runtime.Marshaler, client CustomerServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq AddSocialMediaHandleRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["CustomerID"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "CustomerID")
+	}
+
+	protoReq.CustomerID, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "CustomerID", err)
+	}
+
+	msg, err := client.AddSocialMediaHandle(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 func request_CustomerService_CreateCustomer_0(ctx context.Context, marshaler runtime.Marshaler, client CustomerServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq CreateCustomerRequest
 	var metadata runtime.ServerMetadata
@@ -83,6 +153,46 @@ func RegisterCustomerServiceHandler(ctx context.Context, mux *runtime.ServeMux, 
 // "CustomerServiceClient" to call the correct interceptors.
 func RegisterCustomerServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client CustomerServiceClient) error {
 
+	mux.Handle("POST", pattern_CustomerService_AddAddressToCustomer_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_CustomerService_AddAddressToCustomer_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CustomerService_AddAddressToCustomer_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_CustomerService_AddSocialMediaHandle_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_CustomerService_AddSocialMediaHandle_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CustomerService_AddSocialMediaHandle_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_CustomerService_CreateCustomer_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -107,9 +217,17 @@ func RegisterCustomerServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 }
 
 var (
+	pattern_CustomerService_AddAddressToCustomer_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"v1", "customers", "CustomerID", "address"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_CustomerService_AddSocialMediaHandle_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"v1", "customers", "CustomerID", "social_media_handles"}, "", runtime.AssumeColonVerbOpt(true)))
+
 	pattern_CustomerService_CreateCustomer_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "customers"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
+	forward_CustomerService_AddAddressToCustomer_0 = runtime.ForwardResponseMessage
+
+	forward_CustomerService_AddSocialMediaHandle_0 = runtime.ForwardResponseMessage
+
 	forward_CustomerService_CreateCustomer_0 = runtime.ForwardResponseMessage
 )
